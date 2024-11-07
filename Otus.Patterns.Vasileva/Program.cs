c143f859a5440ecbe9f888e972912f7ff2728beb
AlonaVasileva <vsemposhapke@gmail.com>
2024-11-07 14:58:20 +0300

    fix. Почистила код для проверки

--- Otus.Patterns.Vasileva/Program.cs
+++ Otus.Patterns.Vasileva/Program.cs
@@ -1,11 +1,9 @@
 ﻿using Otus.Patterns.Vasileva;
 
-// Программа для демонстрации паттерна "Прототип"
 class Program
 {
     static void Main(string[] args)
     {
-        // Создаем объекты разных типов лыж
         var freerideSkis = new Freeride("Salomon", 175, "off-piste", "medium");
         var racingSkis = new Racing("Atomic", 180, "race course", "camber");
         var freestyleSkis = new Freestyle("K2", 165, "park", "step-in");
@@ -14,8 +12,7 @@ class Program
         var freerideSkisClone = freerideSkis.Clone();
         var racingSkisClone = racingSkis.Clone();
         var freestyleSkisClone = freestyleSkis.Clone();
-
-        // Выводим оригинальные и клонированные объекты
+        
         Console.WriteLine("Original Freeride Skis: " + freerideSkis);
         Console.WriteLine("Cloned Freeride Skis: " + freerideSkisClone);
         Console.WriteLine("Original Racing Skis: " + racingSkis);
